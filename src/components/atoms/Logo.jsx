809faba9605784fbca6e47265f4469bd471611c0
Joel <joedash77@gmail.com>
2026-05-07 19:27:33 -0300

    fix images

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const Logo = ({ className = "w-16 h-16" }) => (
-  <img 
-    src="/Captura.PNG" 
-    alt="Logo Casa del Alfarero" 
+  <img
+    src={`${import.meta.env.BASE_URL}Captura.PNG`}
+    alt="Logo Casa del Alfarero"
     className={`${className} object-contain mix-blend-multiply`}
   />
 )
-
 export default Logo
